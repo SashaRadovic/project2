5690e22a60ee7053eb6a1f8dcc48ad77df7e7223
SashaRadovic <sasa.a.radovic@gmail.com>
2019-05-15 07:45:36 +0200

    added files

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 import os
 import requests
-
+import time
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
+try:
+    if history:
+        print(history)
+except:
+    history =[]
 
+users={}
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
@@ -12,18 +18,49 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+
+
+
 @socketio.on("welcome")
 def welcome(data):
     username=data['username']
-    print(username)
-    emit('wlc', {"username":username}, broadcast=True )
+    timestamp = time.strftime('%I:%M%p on %b %d, %Y')
+    sid=request.sid
+    new_pair={sid:username}
+    users.update(new_pair)
+    print(users)
+    history.append(timestamp+': '+username+' connected')
+
+    emit('wlc', {"username":username, 'timestamp':timestamp}, broadcast=True, include_self=False )
+
+    emit('restr', {"history":history})
+
+@socketio.on("disconnect")
+def quit():
+    username=users[request.sid]
+    timestamp = time.strftime('%I:%M%p on %b %d, %Y')
+    history.append(timestamp+': '+username+' disconnected')
+    emit('quit_msg', {"username":username, 'timestamp':timestamp}, broadcast=True)
 
 @socketio.on("msg")
 def send_message(data):
     post=data['post']
     username=data['username']
+    timestamp = time.strftime('%I:%M%p on %b %d, %Y')
 
-    emit("send_message", {'post': post,'username':username}, broadcast=True )
+    history.append(timestamp+': '+username+': '+post)
+
+
+    emit("send_message", {'post': post,'username':username, 'timestamp':timestamp}, broadcast=True )
+@socketio.on("user_image")
+def send_image(data):
+    base64=data['base64']
+    username=users[request.sid]
+
+    timestamp = time.strftime('%I:%M%p on %b %d, %Y')
+    history.append(timestamp+': '+username+': ')
+    history.append(base64)
+    emit("send_image", {'base64':base64, 'username':username, 'timestamp':timestamp}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/index.js
+++ static/index.js
@@ -5,13 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     socket.on('connect', () => {
+
+
+
       if (window.localStorage.getItem('username')!==null){
           var username=window.localStorage.getItem('username');
           const li = document.createElement('li');
           li.innerHTML ='Welcome back '+username+'!'
           document.querySelector('#chat-messages').appendChild(li);
        } else{
-           window.localStorage.setItem('username', 'Guest')
+           var guest ='Guest'+ Math.floor((Math.random() * 10000) + 5000);
+           window.localStorage.setItem('username', guest)
            var username=window.localStorage.getItem('username');
            const li = document.createElement('li');
            li.innerHTML ='You are now connected as:  '+username+'!'
@@ -26,24 +30,107 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             const post=document.getElementById('input-message').value;
 
-            const username =window.localStorage.getItem('username')
-
+            const username =window.localStorage.getItem('username');
+            document.getElementById('input-message').value="";
             socket.emit('msg', {'post':post, 'username':username});
     });
     });
 
-    socket.on('wlc', data =>{
+
+
+    socket.on('disconnect', () =>{
+
+
+
+        socket.emit('disconnect', {'username':username});
+    });
+
+   socket.on('quit_msg', data=>{
        const li = document.createElement('li');
-       li.innerHTML = `${data.username} connected`;
+       li.innerHTML = `${data.timestamp}: ${data.username} disconnected`;
 
        document.querySelector('#chat-messages').appendChild(li);
+   });
+
+    socket.on('wlc', data =>{
+
+       const li = document.createElement('li');
+       li.innerHTML = `${data.timestamp}: ${data.username} connected`;
+       document.querySelector('#chat-messages').appendChild(li);
+    });
+
+    socket.on('restr', data =>{
+        var history=data.history;
+        var histLength=history.length;
+        for( var i=0; i<histLength; i++){
+            let li = document.createElement('li');
+            if (history[i].includes("base64")){
+                li.innerHTML = `${'<img src="' + history[i] + '"/>'}`;
+                console.log(history[i]);
+            }
+            else {
+            li.innerHTML = history[i];
+        }
+            document.querySelector('#chat-messages').appendChild(li);
+        }
     });
 
 
      socket.on('send_message', data =>{
        const li = document.createElement('li');
-       li.innerHTML = `${data.username}: ${data.post}`;
+       li.innerHTML = `${data.timestamp}: ${data.username}: ${data.post}`;
 
        document.querySelector('#chat-messages').appendChild(li);
      });
+
+
+
+socket.on('connect', () =>{
+    document.getElementById('imagefile').addEventListener('change', e =>{
+        var width =200;
+        var height=200;
+    var file = e.target.files[0],
+    reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onload = function(evt){
+        //Because of how the file was read,
+        //evt.target.result contains the image in base64 format
+        //Nothing special, just creates an img element
+        //and appends it to the DOM so my UI shows
+        //that I posted an image.
+        //send the image via Socket.io
+
+        const img =new Image();
+        img.src = evt.target.result;
+
+        img.onload = () => {
+        const canvas =document.createElement('canvas');
+        canvas.width=width;
+        canvas.height=height;
+        const ctx=canvas.getContext('2d');
+        ctx.drawImage(img,0,0,width, height);
+        const base64 = ctx.canvas.toDataURL('image/jpeg', 0.5);
+
+
+
+
+
+
+
+
+        socket.emit('user_image',{'base64':base64} );
+};
+    //And now, read the image and base64
+    //reader.readAsDataURL(base64);
+        };
+});
+
+socket.on('send_image', data =>{
+  const li = document.createElement('li');
+  li.innerHTML = `${data.timestamp}: ${data.username}: ${'<img src="' + data.base64 + '"/>'}`;
+
+  document.querySelector('#chat-messages').appendChild(li);
+});
+
+});
 });
