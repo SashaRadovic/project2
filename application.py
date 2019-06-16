d935baefce699034d971b39f70d14628bfa645fd
SashaRadovic <sasa.a.radovic@gmail.com>
2019-06-16 15:50:36 +0200

    a21

--- application.py
+++ application.py
@@ -151,4 +151,6 @@ def gifDisplay(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 33507))
+
+    socketio.run(app, host='0.0.0.0', port=port)
